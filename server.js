e06f0f18d858044af49f015b88037a9a15727f61
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-04-06 21:06:54 +0900

    fix: cors 이슈 해결

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const jsonServer = require("json-server");
 const auth = require("json-server-auth");
+const cors = require("cors");
 
 const app = jsonServer.create();
 const router = jsonServer.router("db.json");
@@ -14,6 +15,7 @@ const rules = auth.rewriter({
 app.db = router.db;
 
 // You must apply the auth middleware before the router
+app.use(cors());
 app.use(rules);
 app.use(auth);
 app.use(router);
